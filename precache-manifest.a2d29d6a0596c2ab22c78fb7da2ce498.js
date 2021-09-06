a04c5da0ba00e7b7d0832746faba2a08aa0ac45b
namita.shenai@gmail.com <namita.shenai@gmail.com>
2021-09-06 00:21:09 -0700

    Updates

---
+++ precache-manifest.a2d29d6a0596c2ab22c78fb7da2ce498.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e25cede337a8a2651f637d35f60bb10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "573c6157363aeaec0092",
+    "url": "/static/css/2.fa6c921b.chunk.css"
+  },
+  {
+    "revision": "f2d13bf52d7bf8861097",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "573c6157363aeaec0092",
+    "url": "/static/js/2.05dd1b72.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/static/js/2.05dd1b72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2d13bf52d7bf8861097",
+    "url": "/static/js/main.35cf338b.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
